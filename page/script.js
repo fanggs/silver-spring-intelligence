4babc1c3a26790b0da0a85109efa96f94eb8d26d
aryannimmagadda <nimmagadda.aryansai@gmail.com>
2026-09-19 01:43:31 -0400

    frontend font fix

--- page/script.js
+++ page/script.js
@@ -1,1 +1,16 @@
-console.log("CivicLens frontend loaded.");
+const navLinks =
+    document.querySelectorAll(".nav-links a");
+
+navLinks.forEach(function(link) {
+
+    link.addEventListener("click", function() {
+
+        navLinks.forEach(function(item) {
+            item.classList.remove("active");
+        });
+
+        link.classList.add("active");
+
+    });
+
+});
