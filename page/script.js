2b1dc7c6c9113c2f91579fc9f371ed2f31d16788
aryannimmagadda <nimmagadda.aryansai@gmail.com>
2026-09-19 01:33:37 -0400

    frontend increment 2

--- page/script.js
+++ page/script.js
@@ -1,12 +1,1 @@
-const button =
-    document.getElementById("searchButton");
-
-button.addEventListener("click", function() {
-
-    const question =
-        document.getElementById("question").value;
-
-    document.getElementById("answer").textContent =
-        "You asked: " + question;
-
-});
+console.log("CivicLens frontend loaded.");
